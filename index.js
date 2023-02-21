5b8d934380547e0424334234b5031029952d8edf
savita-nandan <snandan@tekion.com>
2023-02-21 14:57:42 +0530

    Button and Cart functionality using JS Completed

--- index.js
+++ index.js
@@ -1,71 +1,52 @@
-import {items, categories} from './data.js';
+import {
+    items,
+    categories
+} from './data.js';
+import {
+    createPara,
+    createImage,
+    createBtn,
+    createContainer
+} from './create.js';
 
-function createPara(text, style){
-    const para = document.createElement("p");
-    para.textContent = text;
-    style.forEach(element => {
-        para.classList.add(element);
-    });
-    return para;
-}
-
-function createImage(url, style){
-    const img = document.createElement("img");
-    img.src = url;
-    style.forEach(element => {
-        img.classList.add(element);
-    });
-    return img;
-}
-
-function createBtn(text){
-    const btn = document.createElement("button");
-    btn.textContent = text;
-    return btn;
-}
-
-function createContainer(style){
-    const container = document.createElement("div");
-    style.forEach(element => {
-        container.classList.add(element);
-    });
-    return container;
-}
-
-
-function makeGrid(items){
+function makeGrid(items) {
     const innerDiv = createContainer(["flex"]);
-    const iconDiv = createContainer(["card", "flex",  "flex__column",  "flex__center__cross-axis"]);
+    const iconDiv = createContainer(["card", "flex", "flex__column", "flex__center__cross-axis"]);
     const div1 = createContainer([]);
     const div2 = createContainer(["container__btn", "flex__center__main-axis", "flex__center__cross-axis"]);
     const text = createPara("10% off", []);
     div2.appendChild(text);
     const img = createImage(items.image_url, ["container__icon"]);
-    div1.append(div2,img);
+    div1.append(div2, img);
 
     const para1 = createPara("Sourced at 5 AM", ["para2"]);
     const para2 = createPara(items.product_name, ["para1"]);
     const para3 = createPara("1 kg", ["para1", "gray"]);
 
-    const bottomDiv = createContainer(["cardBottom", "flex",  "flex__center__cross-axis"]);
+    const bottomDiv = createContainer(["cardBottom", "flex", "flex__center__cross-axis"]);
     const div4 = document.createElement("div");
     const p1 = createPara(`₹${items.discount_price}`, []);
     const p2 = createPara(`₹${items.price}`, ["gray"]);
-    div4.append(p1,p2);
+    div4.append(p1, p2);
     const btn = createBtn("ADD");
-    btn.classList.add("innerbtn");
+    btn.classList.add("flex", "flex__center__main-axis", "flex__center__cross-axis", "innerbtn");
+    btn.setAttribute("id", items.id);
+    btn.addEventListener("click", handleCart);
     bottomDiv.append(div4, btn);
 
-    iconDiv.append(div1, para1, para2, para3,bottomDiv);
+    iconDiv.append(div1, para1, para2, para3, bottomDiv);
     innerDiv.append(iconDiv);
 
     return innerDiv;
 }
 
-function makeCategory(category){
+function makeCategory(category) {
     const section = createContainer(["asideMenu", "flex"]);
     const img = createImage(category.image_url, ["asideMenu__icon"]);
     const text = createPara(category.display_text, []);
+    img.setAttribute("id", category.id);
+    text.setAttribute("id", category.id);
+    section.setAttribute("id", category.id);
     section.append(img, text);
     return section;
 }
@@ -74,11 +55,9 @@ function makeCategory(category){
 const gridContainer = document.getElementById("gridContainer");
 gridContainer.classList.add("grid__container", "flex", "flex__column");
 
-for(let i=0;i<items.length;i+=4)
-{
+for (let i = 0; i < items.length; i += 4) {
     const row = createContainer(["flex"]);
-    for(let j=i;j<i+4;j++)
-    {
+    for (let j = i; j < i + 4; j++) {
         const div = makeGrid(items[j]);
         row.append(div);
     }
@@ -90,13 +69,105 @@ const asideBar = document.getElementById("asideBar");
 categories.forEach(element => {
     const category = makeCategory(element);
     asideBar.appendChild(category);
+    asideBar.addEventListener("click", onClick);
 });
-asideBar.appendChild(category);
 
+function removeGrid() {
+    gridContainer.replaceChildren();
+}
 
+function buildGrid(id) {
+    let container = createContainer(["flex"]);
+    for (let i = 0, count = 0; i < items.length; i++) {
+        if (items[i].categories.includes(id)) {
+            const div = makeGrid(items[i]);
+            container.append(div);
+            count++;
+        }
+        if (count === 4) {
+            gridContainer.append(container);
+            count = 0;
+            container = createContainer(['flex']);
+        }
+    }
+    if (container.hasChildNodes())
+        gridContainer.append(container);
+}
 
+// function 
+function onClick(event) {
+    const id = event.target.id;
+    removeGrid();
+    buildGrid(id);
+}
 
+const counter = new Map();
 
+function handleCart(event) {
+    let id = event.target.id;
+    let countDisplay = createPara('0', ["flex"]);
+    countDisplay.setAttribute("id", id);
 
+    if (counter.has(id) == false) {
+        counter.set(id, 0);
+    }
+    countDisplay.innerHTML = counter.get(id);
+    let cartBtn = document.getElementById(id);
+    let addBtn = createPara('+', ["flex"]);
 
+    addBtn.setAttribute("id", id);
+    addBtn.addEventListener("click", addCart);
 
+    let subBtn = createPara('-', ["flex"]);
+    subBtn.setAttribute("id", id);
+
+    cartBtn.replaceChildren(subBtn, countDisplay, addBtn);
+    subBtn.addEventListener("click", subCart);
+    cartBtn.classList.add("flex");
+}
+
+let count = 0;
+const addedToCart = document.getElementById("addedToCart");
+addedToCart.classList.add("flex", "flex__column", "flex__center__cross-axis");
+const p = createPara(count, []);
+
+function addCart(event) {
+    let id = event.target.id;
+
+    count++;
+
+    if (counter.has(id)) {
+        counter.set(id, counter.get(id) + 1);
+    }
+    for (let i = 0; i < items.length; i++) {
+        if (items[i].id == id) {
+            let cart_item = createPara(items[i].product_name, ["flex", "flex__center__cross-axis"]);
+            addedToCart.append(cart_item);
+            break;
+        }
+    }
+    p.replaceChildren(count);
+}
+
+function subCart(event, subBtn) {
+    let id = event.target.id;
+    if (counter.get(id) > 0) {
+        counter.set(id, counter.get(id) - 1);
+        count--;
+    }
+    let item = "";
+    for (let i = 0; i < items.length; i++) {
+
+        if (items[i].id == id) {
+            item = items[i].product_name;
+        }
+    }
+    for (let i = 0; i < addedToCart.children.length; i++) {
+        if (addedToCart.children[i].textContent == item) {
+            addedToCart.removeChild(addedToCart.children[i]);
+            break;
+        }
+    }
+    p.replaceChildren(count);
+}
+addedToCart.append(p);
